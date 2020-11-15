79e3a3c18cd9570886be7e94f8c1ef12d9d64933
John L. Lao <john.lao@gmail.com>
2020-11-15 20:45:02 +0800

    golog

---
+++ articles/index.md
@@ -0,0 +1,2 @@
+# Contents
+- [Trump ignores deepening coronavirus crisis, Biden calls for urgent response](/Trump_ignores_COVID_crisis) 
---
+++ markdown/markdown.go
@@ -0,0 +1,26 @@
+package markdown
+
+import (
+	"bytes"
+
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/renderer/html"
+)
+
+type MarkdownContent []byte
+
+func (c MarkdownContent) Convert() ([]byte, error) {
+	var ren = html.WithUnsafe()
+	var o = goldmark.WithRendererOptions(ren)
+	var ext = goldmark.WithExtensions(extension.Table)
+	var m = goldmark.New(ext, o)
+
+	var err error
+	var w = &bytes.Buffer{}
+	err = m.Convert(c, w)
+	if err != nil {
+		return nil, err
+	}
+	return w.Bytes(), nil
+}
